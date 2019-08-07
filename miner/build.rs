e53008c7fadca551cc4885f87a244a3e740d9233
kilb <github@bll.io>
2019-08-07 14:14:51 +0800

    fix: remove unused build option

--- miner/build.rs
+++ miner/build.rs
@@ -5,7 +5,6 @@ fn main() {
         .file("src/worker/cuckoo.c")
         .include("src/worker")
         .flag("-O3")
-        .flag("-lcrypto")
         .flag("-march=native")
         .static_flag(true)
         .compile("libmean.a");
